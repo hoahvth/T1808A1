eab13d134fcdf9208713ec3cd08b1c08118cd0fd
ductrong24 <44285953+ductrong24@users.noreply.github.com>
2018-10-19 14:51:43 +0700

    Update fghj.c

---
+++ fghj.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void concat(char s1[50], char s2[50]) {
+	
+    printf("\nEnter the first string: "); fflush(stdin); fflush(stdout); gets(s1);
+    printf("\nEnter the second string: "); fflush(stdin); fflush(stdout); gets(s2);
+    strcat(s1, s2);
+	printf("\nThe concatenated string: %s", s1);
+} 
+
+int main(int argc, char *argv[]) {
+	char s1[50];
+	char s2[50];
+	concat(s1,s2);
+	return 0;
+}
+
